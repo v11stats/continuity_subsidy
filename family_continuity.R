01a73f85791af6c210caa586d718bf4da9077402
Michael Wohner <mwohner@gmail.com>
2023-05-24 08:33:53 -0700

    fixed reporting to produce survival curves, the previous commit only produced medians, we are now accounting for right censored observations. Updated spell_arrangement_function to achieve this and fixced report

--- family_continuity.R
+++ family_continuity.R
@@ -1,5 +1,5 @@
 # Rscript to read in excel files and create family spells
-library(readxl); library(tidyverse); library(data.table)
+library(readxl); library(tidyverse); library(data.table); library(survival)
 ################################################################################
 #
 # Add any additional year of data to the below section. Just link the excel file
@@ -16,19 +16,19 @@ x2017 <- read_excel('2017_OSU_Update_202202.xlsx')
 x2018 <- read_excel('2018_OSU_Update_202202.xlsx')
 x2019 <- read_excel('2019_OSU_Update_202202.xlsx')
 # collect all the datasets and only get the columns I want
-d_names <- ls(pattern = "x20[0-9][0-9]+")# names of datasets
-d_list <- mget(d_names)
-for(i in seq_along(d_list)) {
-    names(d_list[[i]]) =
-        tolower(names(d_list[[i]]))
+f_names <- ls(pattern = "x20[0-9][0-9]+")# names of datasets
+f_list <- mget(f_names)
+for(i in seq_along(f_list)) {
+    names(f_list[[i]]) =
+        tolower(names(f_list[[i]]))
 }
-for(i in seq_along(d_list)) {
-    d_list[[i]] <- d_list[[i]] %>% select(adultid_secure, benemonth, fy) %>%
+for(i in seq_along(f_list)) {
+    f_list[[i]] <- f_list[[i]] %>% select(adultid_secure, benemonth, fy) %>%
         distinct()
         }
 rm(list=ls(pattern = "x20[0-9][0-9]+"))
 gc()
-d_names
+f_names
 setwd("~/git/continuity_subsidy")
 # needed
 # this file is required to run this script, it contains a bespoke function
@@ -38,19 +38,19 @@ source("~/git/continuity_subsidy/spell_arrangement_function.R")
 # and all providers in each year, all_providers
 # make a larger dataset of all possible provider month combinations, all_possible
 # any month that a family has no billing will be NA
-for(i in seq_along(d_list)) {
-    all_months <- d_list[[i]] %>% select(benemonth) %>%
+for(i in seq_along(f_list)) {
+    all_months <- f_list[[i]] %>% select(benemonth) %>%
         distinct() %>% arrange(benemonth)
-    all_providers <- d_list[[i]] %>% select(adultid_secure) %>%
+    all_providers <- f_list[[i]] %>% select(adultid_secure) %>%
          distinct() %>% arrange(adultid_secure)
     all_possible <- cross_join(all_months,all_providers)
-    d_list[[i]] <- left_join(all_possible,d_list[[i]])
+    f_list[[i]] <- left_join(all_possible,f_list[[i]])
 }
 # data pairings. groups data into two consecutive years:
 biannual_spells <- list()
-yrs <- str_sub(d_names,2,5)
+yrs <- str_sub(f_names,2,5)
 for(i in 1:(length(yrs)-1)){
-    biannual_spells[[i]] <- bind_rows(d_list[[i]],d_list[[i+1]] )
+    biannual_spells[[i]] <- bind_rows(f_list[[i]],f_list[[i+1]] )
 }
 
 
@@ -58,21 +58,21 @@ durations <- list()
 #get durations
 for(i in seq_along(biannual_spells)) {
     durations[[i]] <- get_spell_length(i,'biannual_spells')
-    summ <-summary(durations[[i]])
-    print(paste0("Summary for years: ",yrs[i]," to ", yrs[i+1]))
-    print(summ)
+   
 }
 # build report
-report <- tibble(year=NA,mean=NA,median=NA,max=NA,min=NA, N_of_famileis=NA, sd=NA)
-for(i in seq_along(biannual_spells)) {
-    temp <- durations[[i]][[2]] 
+report <- tibble(Year=NA,Median=NA,LCL=NA,UCL=NA, N_of_families=NA, Events=NA)
+for(i in seq_along(durations)) {
+    temp <- durations[[i]] 
     report[i,1] <- paste0("Years: ",yrs[i]," - ", yrs[i+1])
-    report[i,2] <- mean(temp)
-    report[i,3] <- median(temp)
-    report[i,4] <- max(temp)
-    report[i,5] <- min(temp)
-    report[i,6] <- length(temp)
-    report[i,7] <- sd(temp)
+    #create surv object
+    tempSurv <- survfit(Surv(temp$sp_length,temp$rcensor)~1)
+    tempQuants <- quantile(tempSurv,probs = .5)
+    report[i,2] <- tempQuants$quantile
+    report[i,3] <- tempQuants$lower
+    report[i,4] <- tempQuants$upper
+    report[i,5] <- tempSurv$n
+    report[i,6] <- sum(tempSurv$n.event)
 }
 report
 save(report, file = 'biannual_spells.rDATA')
